72455f84ad36029c246846691961a44e113342e4
theosherry <sherr.theo@gmail.com>
2021-09-05 23:43:49 +0000

    use flake-utils

--- flake.nix
+++ flake.nix
@@ -3,32 +3,17 @@
 
   inputs = {
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=bad3ccd099ebe9a8aa017bda8500ab02787d90aa";
+
+    flake-utils.url = "github:numtide/flake-utils?rev=98c8d36b1828009b20f12544214683c7489935a1";
 
   };
 
-  outputs = { self, nixpkgs, ... }:
-  let
-    system = "x86_64-linux";
-  in
-  {
-
-    packages."${system}".orgWaitUpon = 
-    with import nixpkgs { inherit system; };
-
-    emacsPackagesNg.melpaBuild {
-      pname = "org-wait-upon";
-      ename = "org-wait-upon";
-      version = "0.10";
-
-      recipe = builtins.toFile "recipe" ''
-        (org-wait-upon :fetcher github
-        :repo "theosherry/org-wait-upon")
-      '';
-      src = ./org-wait-upon.el;
-    };
-
-    defaultPackage."${system}" = self.packages."${system}".orgWaitUpon;
+  outputs = { self, nixpkgs, flake-utils, ... }:
+  flake-utils.lib.simpleFlake {
+    inherit self nixpkgs;
+    name = "org-wait-upon";
+    overlay = ./overlay.nix;
   };
 }
 
