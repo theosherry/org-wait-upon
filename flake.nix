5689c383eff390766126f2ea7de2c83db3f1af9e
theosherry <sherr.theo@gmail.com>
2021-09-28 19:39:23 +0000

    extend custom nix packages instead of overriding

--- flake.nix
+++ flake.nix
@@ -12,29 +12,33 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
   let
     overlay = final: prev: {
-      theoNix.emacsPackages.org-wait-upon = prev.emacsPackagesNg.melpaBuild {
-        pname = "org-wait-upon";
-        ename = "org-wait-upon";
-        version = "0.10";
+      theoNix = (prev.theoNix or {}) // {
+        emacsPackages = (prev.theoNix.emacsPackages or {}) // {
+          org-wait-upon = prev.emacsPackagesNg.melpaBuild {
+            pname = "org-wait-upon";
+            ename = "org-wait-upon";
+            version = "0.10";
 
-        recipe = builtins.toFile "recipe" ''
-          (org-wait-upon :fetcher github
-          :repo "theosherry/org-wait-upon")
-        '';
-        src = ./org-wait-upon.el;
+            recipe = builtins.toFile "recipe" ''
+              (org-wait-upon :fetcher github
+              :repo "theosherry/org-wait-upon")
+            '';
+            src = ./org-wait-upon.el;
+          };
+        };
       };
     };
     sysSpecific = flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ overlay ];
-        };
-        packages = { org-wait-upon = pkgs.theoNix.emacsPackages.org-wait-upon; };
-        defaultPackage = packages.org-wait-upon;
-      in
-        { inherit packages defaultPackage; });
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ overlay ];
+      };
+      packages = { org-wait-upon = pkgs.theoNix.emacsPackages.org-wait-upon; };
+      defaultPackage = packages.org-wait-upon;
     in
-    { inherit overlay; } // sysSpecific;
+    { inherit packages defaultPackage; });
+  in
+  { inherit overlay; } // sysSpecific;
 }
 
